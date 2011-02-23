b688dbf929e0db13747750702013800758acf25e
phpprof <phpprof@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-23 08:57:07 +0000

    [-FEATURE] Removed ext tv_alttemplatefolder (TV can do this on its own)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3dummy/trunk@44099 735d13b6-9817-0410-8766-e36946ffe9aa

--- typo3conf/ext/tv_alttemplatefolder/ext_localconf.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
-
-$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/templavoila/mod2/index.php'] = t3lib_extMgm::extPath('tv_alttemplatefolder') . '/Classes/XClass/class.ux_tx_templavoila_module2.php';
-
-
-?>
