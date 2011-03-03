736d618032dab641f37461f906ac0b3e8fdd2854
phpprof <phpprof@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-03 16:52:14 +0000

    [+TASK] Added Configuration for JPEG Quality 100 [~TASK] Cleaned up localconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3dummy/trunk@44583 735d13b6-9817-0410-8766-e36946ffe9aa

--- typo3conf/localconf.php
+++ typo3conf/localconf.php
@@ -17,8 +17,11 @@ $TYPO3_CONF_VARS['GFX']['im_path_lzw']  = 'C:\\Program Files\\ImageMagick-6.6.7-
 $typo_db_extTableDef_script = 'extTables.php';
 
 $TYPO3_CONF_VARS['GFX']['gdlib_2']      = '1';
+$TYPO3_CONF_VARS['GFX']['gdlib_png'] = '1';
 $TYPO3_CONF_VARS['GFX']['im_version_5'] = 'im6';
-$TYPO3_CONF_VARS['GFX']['TTFdpi']       = '96';
+$TYPO3_CONF_VARS['GFX']['jpg_quality'] = '100';
+$TYPO3_CONF_VARS['GFX']['TTFdpi'] = '96';
+$TYPO3_CONF_VARS['GFX']['png_truecolor'] = '1';
 
 $TYPO3_CONF_VARS['SYS']['t3lib_cs_convMethod'] = 'mbstring';
 $TYPO3_CONF_VARS['SYS']['t3lib_cs_utils']      = 'mbstring';
@@ -44,10 +47,6 @@ $GLOBALS['TYPO3_CONF_VARS']['FE']['addAllowedPaths'] = 'typo3/gfx/';
 $TYPO3_CONF_VARS['EXT']['extList'] = 'pagetree,extbase,css_styled_content,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,t3skin,opendocs,openid,nc_staticfilecache,realurl,aeurltool,nwt_imagecrop,oneclicklogin,info,perm,func,filelist,t3editor,reports,fluid,static_info_tables,linkvalidator,templavoila';	// Modified or inserted by TYPO3 Extension Manager. Modified or inserted by TYPO3 Core Update Manager.
 $TYPO3_CONF_VARS['EXT']['extList_FE'] = 'extbase,css_styled_content,install,rtehtmlarea,t3skin,openid,nc_staticfilecache,realurl,aeurltool,nwt_imagecrop,oneclicklogin,fluid,static_info_tables,linkvalidator,templavoila';	// Modified or inserted by TYPO3 Extension Manager.
 
-$TYPO3_CONF_VARS['GFX']['gdlib_png'] = '1';	// Modified or inserted by TYPO3 Install Tool. 
-$TYPO3_CONF_VARS['GFX']['png_truecolor'] = '1';	//  Modified or inserted by TYPO3 Install Tool.
-
-$TYPO3_CONF_VARS['FE']['disableNoCacheParameter'] = '0';	//  Modified or inserted by TYPO3 Install Tool.  
 $TYPO3_CONF_VARS['EXT']['extConf']['realurl'] = 'a:5:{s:10:"configFile";s:26:"typo3conf/realurl_conf.php";s:14:"enableAutoConf";s:1:"1";s:14:"autoConfFormat";s:1:"0";s:12:"enableDevLog";s:1:"0";s:19:"enableChashUrlDebug";s:1:"0";}';	//  Modified or inserted by TYPO3 Extension Manager. 
 
 
